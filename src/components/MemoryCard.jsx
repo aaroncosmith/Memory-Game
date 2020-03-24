8b17de2f61667b0935f95b9b6b7ab8f259a55973
Aaron Smith <aaroncosmith@gmail.com>
2020-03-24 11:17:00 -0400

    added memorycard component

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+class MemoryCard exrends Component {
+    render() {
+        return (
+            <h1>CARD</h1>
+        )
+    }
+}
+
+export default MemoryCard;
