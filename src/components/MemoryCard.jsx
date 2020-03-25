1836a5980399f1206dcd697417a0d40590abbaab
Aaron Smith <aaroncosmith@gmail.com>
2020-03-25 11:53:57 -0400

    changed classname

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -5,7 +5,7 @@ import './MemoryCard.css';
 class MemoryCard extends Component {
     render() {
         return (
-            <div className="MemoryCard">
+            <div className="MemoryCardBack">
                 <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""></img>
             </div>
         );
