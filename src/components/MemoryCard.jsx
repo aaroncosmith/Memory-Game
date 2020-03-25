18b5ef272cb3d58231cffd44c80748aa8dc12f95
Aaron Smith <aaroncosmith@gmail.com>
2020-03-25 13:14:16 -0400

    added more card handlers'

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -6,8 +6,13 @@ class MemoryCard extends Component {
     render() {
         return (
             <div className="MemoryCard">
-                <div className="MemoryCardBack">
-                    <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""></img>
+                <div className="MemoryCardInner">
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""></img>
+                    </div>
+                    <div className="MemoryCardFront">
+                        ∆
+                    </div>
                 </div>
             </div>
         );
